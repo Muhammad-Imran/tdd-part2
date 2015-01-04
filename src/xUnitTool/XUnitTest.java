0477f47bd2b75e7ad15068a0f9e289d58808d6a3
Muhammad-Imran <muhammad.imran@globalnorthstar.com>
2015-01-04 14:46:06 +0500

    Chapter 21

--- src/xUnitTool/XUnitTest.java
+++ src/xUnitTool/XUnitTest.java
@@ -4,8 +4,9 @@ public class XUnitTest {
 
 	public static void main(String[] args) {
 		try {
-			new TestCaseTest("testTemplateMethod").run();
+			new TestCaseTest("testFailedResult").run();
 		} catch (Exception e) {
+			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
