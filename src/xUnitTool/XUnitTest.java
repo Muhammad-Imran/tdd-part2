7f498ec2617981843c09feec5ffd78456d8f6fa7
Muhammad-Imran <muhammad.imran@globalnorthstar.com>
2015-01-04 16:16:48 +0500

    Chapter 23

--- src/xUnitTool/XUnitTest.java
+++ src/xUnitTool/XUnitTest.java
@@ -4,9 +4,18 @@ public class XUnitTest {
 
 	public static void main(String[] args) {
 		try {
-			new TestCaseTest("testFailedResultFormatting").run();
+			TestSuite suite = new TestSuite();
+			TestResult result = new TestResult();
+			
+			suite.add(new TestCaseTest("testTemplateMethod"));
+			suite.add(new TestCaseTest("testResult"));
+			suite.add(new TestCaseTest("testFailedResultFormatting"));
+			suite.add(new TestCaseTest("testFailedResult"));
+			suite.add(new TestCaseTest("testSuite"));
+			
+			suite.run(result);
+			System.out.println(result.summary());
 		} catch (Exception e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
